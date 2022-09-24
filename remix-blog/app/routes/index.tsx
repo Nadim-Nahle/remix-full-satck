448da9eb5c78d343699dd8ea78cfa2ce11e77132
Nadim <nahlenadim@gmail.com>
2022-09-24 13:01:49 +0300

    Remix Create Login Form

--- remix-blog/app/routes/index.tsx
+++ remix-blog/app/routes/index.tsx
@@ -1,8 +1,9 @@
 function index() {
   return (
     <div className="h-screen w-full bg-slate-600">
-      <h1 className="font-bold text-5xl text-blue-400">weclome</h1>
-      <p>my first remix project</p>
+      <h1 className="font-bold text-5xl text-blue-400">
+        my first remix project
+      </h1>
     </div>
   );
 }
