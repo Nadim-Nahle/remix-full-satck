87eda81926e99f714969d2d3fd0662fd82a585bb
Nadim <nahlenadim@gmail.com>
2022-09-24 20:25:28 +0300

    Remix Create User Circle

--- remix-blog/app/components/user-panel.tsx
+++ remix-blog/app/components/user-panel.tsx
@@ -3,7 +3,7 @@ import { User } from "@prisma/client";
 interface props {
   users: User[];
 }
-export function UserPanel() {
+export function UserPanel({ users }: props) {
   return (
     <div className="w-1/6 bg-gray-200 flex flex-col">
       <div className="text-center bg-gray-300 h-20 flex items-center justify-center">
