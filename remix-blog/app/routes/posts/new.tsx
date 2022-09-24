7be309d371c317deea09618a31695c5792ccfcce
Nadim <nahlenadim@gmail.com>
2022-09-24 10:02:19 +0300

    Remix Create Form

--- remix-blog/app/routes/posts/new.tsx
+++ remix-blog/app/routes/posts/new.tsx
@@ -1,8 +1,29 @@
+import { Link } from "@remix-run/react";
+
 function NewPost() {
   return (
-    <div>
-      <h1>New Post</h1>
-    </div>
+    <>
+      <div className="page-header">
+        <h1>New Post</h1>
+        <Link to="/posts" className="btn btn-reverse">
+          Back
+        </Link>
+      </div>
+
+      <div className="page-content">
+        <form method="POST">
+          <div className="form-control">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" />
+            <label htmlFor="body">post body</label>
+            <textarea name="body" id="body" />
+          </div>
+          <button className="btn btn-block" type="submit">
+            Add post
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
 
