57f7d2d9b6962f8900025d7fef3f1cfec9b5326b
Nadim <nahlenadim@gmail.com>
2022-09-24 17:00:55 +0300

    Remix Create Get Logout function

--- remix-blog/app/utils/auth.server.ts
+++ remix-blog/app/utils/auth.server.ts
@@ -106,4 +106,24 @@ export async function getUser(request: Request){
     if(typeof userId != 'string'){
         return null
     }
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {id: userId},
+            select: {id: true, email: true, profile:true}
+        })
+        return user
+    } catch  {
+        throw logout(request)
+    }
+}
+
+export async function logout(request:Request) {
+
+    const session = await getUserSession(request)
+    return redirect("/login",{
+        headers: {
+            'set-Cookie': await storage.destroySession(session),    
+        }
+    })
 }
