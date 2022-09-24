3c1af0e7a2c4410b5772af5b4a579e999e95f583
Nadim <nahlenadim@gmail.com>
2022-09-24 14:52:41 +0300

    Remix Create auth server

--- remix-blog/app/utils/auth.server.ts
+++ remix-blog/app/utils/auth.server.ts
@@ -1,6 +1,18 @@
+import { json } from '@remix-run/node'
 import {prisma} from './prisma.server'
 import { RegisterForm } from './types.server'
 
 export const register = async (form: RegisterForm) => {
+    const exists = await prisma.user.count({where: {email: form.email}})
 
+    if (exists){
+        return json(
+            {
+                error: `user already exits`
+            },
+            {
+                status: 400
+            }
+        )
+    }
 }
