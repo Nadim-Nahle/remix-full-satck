d9d0308f8d8432ae4604744ed8093f3f9f13c015
Nadim <nahlenadim@gmail.com>
2022-09-24 11:17:56 +0300

    Remix Install Prisma

--- remix-blog/app/routes/index.tsx
+++ remix-blog/app/routes/index.tsx
@@ -1,7 +1,7 @@
 function index() {
   return (
-    <div>
-      <h1>weclome</h1>
+    <div className="h-screen w-full bg-slate-600">
+      <h1 className="font-bold text-5xl text-blue-400">weclome</h1>
       <p>my first remix project</p>
     </div>
   );
