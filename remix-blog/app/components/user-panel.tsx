809ee804e77d234a93b960eb40b3b1cfb7cd4d9c
Nadim <nahlenadim@gmail.com>
2022-09-24 20:05:47 +0300

    Remix Import update User panel

--- remix-blog/app/components/user-panel.tsx
+++ remix-blog/app/components/user-panel.tsx
@@ -1,7 +1,7 @@
 export function UserPanel() {
   return (
     <div className="w-1/6 bg-gray-200 flex flex-col">
-      <div className="text-center bg-gray-300 h-200 flex items-center justify-center">
+      <div className="text-center bg-gray-300 h-20 flex items-center justify-center">
         <h2 className="text-xl text-blue-600 font-semibold">My Team</h2>
       </div>
       <div className="flex-1 overflow-y-scroll py-4 flex flex-col gap-y-10">
