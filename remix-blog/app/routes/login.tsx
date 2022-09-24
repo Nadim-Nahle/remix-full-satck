0f7e1b0b59d4258f725f4c98f9aa95b2cf9f7784
Nadim <nahlenadim@gmail.com>
2022-09-24 13:43:12 +0300

    Remix Create Action state

--- remix-blog/app/routes/login.tsx
+++ remix-blog/app/routes/login.tsx
@@ -26,7 +26,9 @@ function Login() {
         <h2 className="text-5xl font-extrabold text-yellow-300">
           Welcome to Remix!
         </h2>
-        <p className="font-semibold text-slate-300">Please Login</p>
+        <p className="font-semibold text-slate-300">
+          {action == "login" ? "Please Sign In" : "please Sign Up"}
+        </p>
         <form className="rounded-2xl bg-gray-200 p-6 w-96">
           <FormField
             htmlFor="email"
@@ -45,6 +47,7 @@ function Login() {
             <input
               type="submit"
               className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+              value={action == "login" ? "Sign In" : "Sign Up"}
             />
           </div>
         </form>
