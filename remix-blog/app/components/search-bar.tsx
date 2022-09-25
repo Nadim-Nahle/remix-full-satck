3bfa4b4e9c22445a28e3bb492b8e0b66b2704a74
Nadim <nahlenadim@gmail.com>
2022-09-25 13:32:15 +0300

    Remix Import SVG

--- remix-blog/app/components/search-bar.tsx
+++ remix-blog/app/components/search-bar.tsx
@@ -8,14 +8,16 @@ export function SearchBar() {
           className="w-full rounded-xl px-3 py-2"
           placeholder="Search a messageor a name"
         />
+
         <svg
           className="w-4 h-4 fill-current text-gray-400 -ml-8"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          height="1em"
-          width="1em"
+          fill="#000000"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 30 30"
+          width="30px"
+          height="30px"
         >
-          <path d="M18.59 13H3a1 1 0 010-2h15.59l-5.3-5.3a1 1 0 111.42-1.4l7 7a1 1 0 010 1.4l-7 7a1 1 0 01-1.42-1.4l5.3-5.3z" />
+          <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z" />
         </svg>
       </div>
     </form>
