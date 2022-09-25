a5a2e858c55fb035aeed540d5fe50f88df24b7d4
Nadim <nahlenadim@gmail.com>
2022-09-25 08:58:17 +0300

    Remix Create Portal Modal

---
+++ remix-blog/app/components/modal.tsx
@@ -0,0 +1,30 @@
+import { useNavigate } from "@remix-run/react";
+import { Portal } from "./portal";
+
+interface props {
+  children: React.ReactNode;
+  isOpen: boolean;
+  ariaLabel?: string;
+  className?: string;
+}
+
+export const Modal: React.FC<props> = ({
+  children,
+  isOpen,
+  ariaLabel,
+  className,
+}) => {
+  const navigate = useNavigate();
+  if (!isOpen) return null;
+  return (
+    <Portal wrapperId="modal">
+      <div
+        className="fixed inset-0 overflow-y-auto bg-gray-600 bg-opacity-80"
+        aria-labelledby={ariaLabel ?? "modal-title"}
+        role="dialog"
+        aria-modal="true"
+        onClick={() => navigate("/home")}
+      ></div>
+    </Portal>
+  );
+};
