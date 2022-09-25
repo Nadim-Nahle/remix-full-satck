8439f830ca5e639b0678c31a5aaf4d0e2300cc8e
Nadim <nahlenadim@gmail.com>
2022-09-25 12:55:28 +0300

    Remix import usercircle

--- remix-blog/app/components/recent-bar.tsx
+++ remix-blog/app/components/recent-bar.tsx
@@ -1,4 +1,5 @@
 import { Kudo, User } from "@prisma/client";
+import { UserCircle } from "./user-circle";
 
 interface KudoWithRecipient extends Kudo {
   recipient: User;
@@ -6,6 +7,20 @@ interface KudoWithRecipient extends Kudo {
 
 export function RecentBar({ kudos }: { kudos: KudoWithRecipient[] }) {
   return (
-    <div className="w-1/5 border-l-4 border-l-yellow-300 flex flex-col items-center"></div>
+    <div className="w-1/5 border-l-4 border-l-yellow-300 flex flex-col items-center">
+      <h2 className="text-xl text-yellow-300 font-semibold my-6">
+        Recent Kudos
+      </h2>
+      <div className="h-full flex flex-col gap-y-10 mt-10">
+        {kudos.map((kudo) => (
+          <div className="h-24 w-24 relative" key={kudo.recipient.id}>
+            <UserCircle
+              profile={kudo.recipient.profile}
+              className="w-20 h-20"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
