04a3f93992b1d2c32a7c9ef26e2586c3c865700a
Nadim <nahlenadim@gmail.com>
2022-09-25 15:10:14 +0300

    Remix Import Loader Function

--- remix-blog/app/routes/home/profile.tsx
+++ remix-blog/app/routes/home/profile.tsx
@@ -1,6 +1,6 @@
 import { Modal } from "~/components/modal";
 
-export function ProfileModal() {
+export default function ProfileModal() {
   return (
     <Modal isOpen={true}>
       <p>Profile settings</p>
