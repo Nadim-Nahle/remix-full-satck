96d3d840b126222920c97aa3ee4aeba73a8df238
Nadim <nahlenadim@gmail.com>
2022-09-25 08:44:06 +0300

    Remix Create Portal

--- remix-blog/app/components/portal.tsx
+++ remix-blog/app/components/portal.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 interface props {
   children: React.ReactNode;
   wrapperId: string;
@@ -9,3 +11,18 @@ const createWrapper = (wrapperId: string) => {
   document.body.appendChild(wrapper);
   return wrapper;
 };
+
+export const Portal: React.FC<props> = ({chidlren, wrapperId}) => {
+    const [wrapper, setWrapper] = useState<HTMLElement | null>(null);
+    useEffect(()=>{
+        let element = document.getElementById(wrapperId)
+        let created = false;
+        if (created && element?.parentNode){
+            element.parentNode.removeChild(element)
+        }
+    },[wrapperId])
+
+    if(wrapper == null)
+
+    
+}
