fae029e54ad69dc984a8828f2b773d853bb15b27
Nadim <nahlenadim@gmail.com>
2022-09-25 10:57:23 +0300

    Remix Import profile

--- remix-blog/app/components/kudo.tsx
+++ remix-blog/app/components/kudo.tsx
@@ -1,5 +1,8 @@
-import { Profile } from "@prisma/client";
+import { Profile, Kudo as IKudo } from "@prisma/client";
 
 interface props {
   profile: Profile;
+  kudo: Partial<IKudo>;
 }
+
+export function Kudo({});
