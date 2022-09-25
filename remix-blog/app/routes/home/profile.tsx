70c7490133af2c51653584b07764db67fc31ca51
Nadim <nahlenadim@gmail.com>
2022-09-25 20:52:49 +0300

    Remix Add Form Action

--- remix-blog/app/routes/home/profile.tsx
+++ remix-blog/app/routes/home/profile.tsx
@@ -65,7 +65,7 @@ export default function ProfileModal() {
         <div className="flex">
           <div className="w-1/3">{/*image uploader*/}</div>
           <div className="flex-1">
-            <form>
+            <form method="post">
               <FormField
                 htmlFor="firstName"
                 label="First Name"
