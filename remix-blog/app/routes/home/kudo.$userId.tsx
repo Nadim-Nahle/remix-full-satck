94f6d59d4cbfb68bb3eac1b6f64cdd4f671b661d
Nadim <nahlenadim@gmail.com>
2022-09-25 09:24:10 +0300

    Remix Create loader function

--- remix-blog/app/routes/home/kudo.$userId.tsx
+++ remix-blog/app/routes/home/kudo.$userId.tsx
@@ -1,17 +1,24 @@
-import { redirect } from "@remix-run/node";
+import { json, LoaderFunction, redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import { Modal } from "~/components/modal";
+import { getUserById } from "~/utils/users.server";
 
 const loader: LoaderFunction = async ({ params }) => {
   const { userId } = params;
   if (typeof userId != "string") {
     return redirect("/home");
   }
+
+  const user = await getUserById(userId);
+  return json({ user });
 };
 
-export default function kudoModal() {
+export default function KudoModal() {
+  const { user } = useLoaderData();
   return (
     <Modal isOpen={true}>
       <h2>this is a modal</h2>
+      <h2>{user.profile.firstName}</h2>
     </Modal>
   );
 }
