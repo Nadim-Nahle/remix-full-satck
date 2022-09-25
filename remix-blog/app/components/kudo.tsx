f5f3c415977fe5b6f95cd14e659d90aaa2c91a53
Nadim <nahlenadim@gmail.com>
2022-09-25 11:00:59 +0300

    Remix Import profile

--- remix-blog/app/components/kudo.tsx
+++ remix-blog/app/components/kudo.tsx
@@ -1,8 +1,17 @@
 import { Profile, Kudo as IKudo } from "@prisma/client";
+import { backgroundColorMap } from "~/utils/constants";
 
 interface props {
   profile: Profile;
   kudo: Partial<IKudo>;
 }
 
-export function Kudo({});
+export function Kudo({ profile, kudo }: props) {
+  return (
+    <div
+      className={`flex ${
+        backgroundColorMap[kudo.style?.backgroundColor || "RED"]
+      } p-4 rounded-xl w-full gap-x-2 relative`}
+    ></div>
+  );
+}
