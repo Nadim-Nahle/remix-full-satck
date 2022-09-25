8793b6b054f36b115983504fae3f65a5c70818b0
Nadim <nahlenadim@gmail.com>
2022-09-25 12:52:13 +0300

    Remix import Kudo from prisma client

--- remix-blog/app/components/recent-bar.tsx
+++ remix-blog/app/components/recent-bar.tsx
@@ -1,5 +1,11 @@
-import { User } from "@prisma/client";
+import { Kudo, User } from "@prisma/client";
 
 interface KudoWithRecipient extends Kudo {
   recipient: User;
+}
+
+export function RecentBar({ kudos }: { kudos: KudoWithRecipient[] }) {
+  return (
+    <div className="w-1/5 border-l-4 border-l-yellow-300 flex flex-col items-center"></div>
+  );
 }
