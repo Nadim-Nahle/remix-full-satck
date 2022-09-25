0f060bfc93efb6547652336c640a705e9fe65acc
Nadim <nahlenadim@gmail.com>
2022-09-25 13:02:06 +0300

    Remix import getRecentKudos

--- remix-blog/app/routes/home.tsx
+++ remix-blog/app/routes/home.tsx
@@ -4,7 +4,7 @@ import { requireUserId } from "~/utils/auth.server";
 import { UserPanel } from "~/components/user-panel";
 import { getOtherUsers } from "~/utils/users.server";
 import { Outlet, useLoaderData } from "@remix-run/react";
-import { getFilteredKudos } from "~/utils/kudo.server";
+import { getFilteredKudos, getRecentKudos } from "~/utils/kudo.server";
 import { Kudo } from "~/components/kudo";
 import { Kudo as Ikudo, Profile } from "@prisma/client";
 
@@ -12,7 +12,8 @@ export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request);
   const users = await getOtherUsers(userId);
   const kudos = await getFilteredKudos(userId, {}, {});
-  return json({ users, kudos });
+  const recentKudos = await getRecentKudos();
+  return json({ users, kudos, recentKudos });
 };
 
 interface KudoWithAuthor extends Ikudo {
