03e04b041bf06df087b0070bd8102c0099580422
Nadim <nahlenadim@gmail.com>
2022-09-25 15:11:45 +0300

    Remix Import getuser Function

--- remix-blog/app/routes/home/profile.tsx
+++ remix-blog/app/routes/home/profile.tsx
@@ -1,4 +1,11 @@
+import { LoaderFunction } from "@remix-run/node";
 import { Modal } from "~/components/modal";
+import { getUser } from "~/utils/auth.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await getUser(request);
+  return json({ user });
+};
 
 export default function ProfileModal() {
   return (
