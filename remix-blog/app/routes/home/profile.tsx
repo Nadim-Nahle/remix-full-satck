d94c70ed9bf8c068b110ecbe384aca974d76fb87
Nadim <nahlenadim@gmail.com>
2022-09-25 15:17:43 +0300

    Remix Import Form FIeld

--- remix-blog/app/routes/home/profile.tsx
+++ remix-blog/app/routes/home/profile.tsx
@@ -1,4 +1,6 @@
 import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import FormField from "~/components/form-field";
 import { Modal } from "~/components/modal";
 import { getUser } from "~/utils/auth.server";
 
@@ -8,9 +10,23 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function ProfileModal() {
+  const { user } = useLoaderData();
+
   return (
-    <Modal isOpen={true}>
-      <p>Profile settings</p>
+    <Modal isOpen={true} className="w-1/3">
+      <div className="p-3">
+        <h2 className="text-4xl font-semibold text-blue-600 text-center mb-4">
+          Your Profile
+        </h2>
+        <div className="fex">
+          <div className="w-1/3">{/*image uploader*/}</div>
+          <div className="flex-1">
+            <form>
+              <FormField />
+            </form>
+          </div>
+        </div>
+      </div>
     </Modal>
   );
 }
